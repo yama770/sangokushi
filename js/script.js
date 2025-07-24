76d06ba634ddb891dbba0dc41529a648025b9276
yama <ym770.programming@gmail.com>
2025-07-25 00:58:38 +0900

    create review section

--- js/script.js
+++ js/script.js
@@ -5,12 +5,10 @@ $(document).ready(function() {
         slidesToScroll: 1,      // 1枚ずつスクロール
         autoplay: true,         // 自動再生
         autoplaySpeed: 3000,    // 3秒間隔
-        dots: true,             // ドット表示
-        arrows: true,           // 矢印表示
         infinite: true,         // 無限ループ
         centerMode: true,      // センターモード有効
         centerPadding: '15%',   // センターモードのパディング
-        variableWidth: false,   // 可変幅無効
+        variableWidth: true,   // 可変幅有効
         responsive: [
             {
                 breakpoint: 768,    // タブレット
