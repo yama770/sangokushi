d90fd6576f2d7dd8bff29f89952ec10e343f025e
yama <ym770.programming@gmail.com>
2025-07-30 22:16:26 +0900

    GSAP test

--- js/ScrollTrigger.js
+++ js/ScrollTrigger.js
@@ -1,4 +1,38 @@
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
+
+// main
+const sections = document.querySelectorAll(".section");
+sections.forEach((section) => {
+  const pin = section.querySelector(".section__pin");
+  const height = section.getBoundingClientRect().height;
+
+  gsap.set(pin, {
+    height: height,
+  });
+
+  ScrollTrigger.create({
+    trigger: section,
+    start: "bottom bottom",
+    // わかりやすいようにマーカーを表示。本番環境では削除してください！
+    markers: true,
+    onEnter: () => {
+      gsap.set(pin, {
+        position: "fixed",
+        top: "auto",
+        bottom: 0,
+        left: 0,
+      });
+    },
+    onLeaveBack: () => {
+      gsap.set(pin, {
+        position: "absolute",
+        top: 0,
+        bottom: "auto",
+        left: 0,
+      });
+    },
+  });
+});
 
 // const sections = document.querySelectorAll("#MetaStep01, #MetaStep02, #MetaStep03");
 
